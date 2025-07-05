850135f145ffd99d40fd69b6c738a99301f6f87c
Hüseyin Dinçbay <hdincbay3@gmail.com>
2025-07-05 16:00:36 +0300

    Added JobSchedule service to the project for background job scheduling and execution.

---
+++ LibraryProject.JOBScheduleService/Job.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.JOBScheduleService
+{
+    public class Job : IJob
+    {
+        private readonly ILogger<Job> _logger;
+
+        public Job(ILogger<Job> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                _logger.LogInformation("Running...");
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: " + ex.ToString());
+            }
+            
+        }
+    }
+}
---
+++ LibraryProject.JOBScheduleService/Program.cs
@@ -0,0 +1,29 @@
+﻿using Quartz;
+using LibraryProject.JOBScheduleService;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Quartz job tanımı
+builder.Services.AddQuartz(q =>
+{
+    var jobKey = new JobKey("DeliveryNotificationCreate");
+
+    q.AddJob<Job>(opts => opts.WithIdentity(jobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(jobKey)
+        .WithIdentity("DeliveryNotificationTrigger")
+        .WithSimpleSchedule(x => x
+            .WithIntervalInSeconds(5)
+            .RepeatForever()));
+});
+
+builder.Services.AddQuartzHostedService(opt => opt.WaitForJobsToComplete = true);
+
+builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddLogging();
+
+var host = builder.Build();
+
+await host.RunAsync();
